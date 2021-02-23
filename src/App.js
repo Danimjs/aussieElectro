8c2433701adb6f57a24e70ec9e273783bea833f9
Danimjs <77038782+Danimjs@users.noreply.github.com>
2021-02-23 16:35:25 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import ScrollyTeller from './Components.ScrollyTeller";
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import { render } from '@testing-library/react';
 
 function App() {
-
+  return <>
+    <ScrollyTeller />
+    </>;
+}
   
 export default App;
